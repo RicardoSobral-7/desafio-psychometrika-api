c4039de4a9bbd4a6f77f2b97162158bb8a59c0ff
RicardoSobral-7 <riki.sobral@gmail.com>
2021-01-20 16:05:53 -0300

    db-questoes

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-    baseURL: "http://localhost:5550"
+    baseURL: "http://localhost:5550/api/"
 });
 
 api.interceptors.request.use(async config => {

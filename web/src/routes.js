0b9537c0e223bca6e89c8bf96256389e403fbe82
RicardoSobral-7 <riki.sobral@gmail.com>
2021-02-04 14:08:56 -0300

    rotas frontend

--- web/src/routes.js
+++ web/src/routes.js
@@ -4,9 +4,6 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages/Home"
 import Signup from "./pages/Signup";
 import Questoes from "./pages/Questoes";
-import Questao2 from "./pages/Questao2";
-import Questao3 from "./pages/Questao3";
-import Questao4 from "./pages/Questao4";
 import Gabarito from "./pages/Gabarito";
 import Ranking from "./pages/Ranking";
 import Evidence from "./pages/Evidence";
@@ -36,9 +33,6 @@ const Routes = () => (
             <Route path="/" exact component={Home} />
             <Route path="/signup" component={Signup} />
             <PrivateRoute path="/prova/questoes" component={Questoes}/>
-            <PrivateRoute path="/prova/questao2" component={Questao2}/>
-            <PrivateRoute path="/prova/questao3" component={Questao3}/>
-            <PrivateRoute path="/prova/questao4" component={Questao4}/>
             <PrivateRoute path="/prova/gabarito" component={Gabarito}/>
             <Route path="/ranking" component={Ranking} />
             <Route path="/evidence/:id" component={Evidence} />
